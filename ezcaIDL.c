4b060751d7ea4fd82ceed4a737b394c1aa1c9225
Janet B. Anderson <jba@aps.anl.gov>
2007-05-24 15:06:14 -0500

    Added include for tsDefs.h.

--- ezcaIDL.c
+++ ezcaIDL.c
@@ -6,7 +6,6 @@
 * This file is distributed subject to a Software License Agreement found
 * in the file LICENSE that is included with this distribution. 
 \*************************************************************************/
-#include <tsDefs.h>
 #include <cadef.h>
 #include <epicsTypes.h>
 #include <db_access.h>
